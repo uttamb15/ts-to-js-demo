56122e1311f2935b1bbe1b7344e6a1bcf9e513a0
Uttam Sethi <uttamsbaba10@gmail.com>
2026-03-02 11:32:58 +0530

    Update lambda_function.ts (#18) (#20)

--- lambda_function.ts
+++ lambda_function.ts
@@ -6,32 +6,11 @@ export const handler = async (
   return {
     statusCode: 200,
     body: JSON.stringify({
-
       message: "Hello from github currenttime : 11:19"
 
     })
   };
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
